3c361ad9f70293c94d7d9c7cc03b2f3f71d8d37a
James Rouzier <rouzier@gmail.com>
2017-07-18 01:51:13 -0400

    Init header file

---
+++ include/ono/flexb.h
@@ -0,0 +1,81 @@
+#ifndef __ONO_FLEXBUFFERS__
+#define __ONO_FLEXBUFFERS__
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <assert.h>
+
+#define FLEXB_NULL  0
+#define FLEXB_INT   1
+#define FLEXB_UINT  2
+#define FLEXB_FLOAT 3
+
+typedef struct ONO_flexb_ref {
+    const uint8_t * data;
+    uint8_t parent_width;
+    uint8_t byte_width;
+    uint8_t type;
+} ONO_flexb_ref;
+
+inline int ono_flexb_set_root( const char* root, size_t length, ONO_flexb_ref* ref) {
+    if (root == NULL || ref == NULL || length < 3) {
+        return EINVAL;
+    }
+
+    const uint8_t* end = root + length;
+    const uint8_t byte_width = *--end;
+    const uint8_t packed_byte = *--end;
+    ref->byte_width = byte_width;
+    ref->type = (packed_byte >> 2);
+    ref->parent_width  = 1 << (packed_byte & 0x03);
+    ref->data = end - byte_width;
+    return 0;
+}
+
+inline int64_t ono_flexb_get_int64(const char* data, int width) {
+    int64_t num;
+    switch (width) {
+    case 1:
+        {
+           int8_t tmp;
+           memcpy(&tmp, data, 1);
+           num = (int64_t) tmp;
+        }
+        return ;
+    case 2:
+        {
+           int16_t tmp;
+           memcpy(&tmp, data, 2);
+           num = (int64_t) tmp;
+        }
+        return 0;
+    case 4:
+        {
+           int32_t tmp;
+           memcpy(&tmp, data, 4);
+           num = (int64_t) tmp;
+        }
+        return 0;
+    case 8:
+        {
+           memcpy(&num, data, 8);
+        }
+        return 0;
+    default:
+        assert(0);
+    }
+    return num;
+}
+
+inline int ono_flexb_as_int64(ONO_flexb_ref* ref, int64_t *num) {
+    switch(ref->type) {
+    case FLEXB_INT:
+        *num = ono_flexb_get_int64(ref->data, ref->parent_width);
+        return 0;
+    }
+    return 1;
+}
+
+#endif
