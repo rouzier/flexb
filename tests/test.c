61940835fde6fe7daa404d7da53210eeea990865
James Rouzier <rouzier@gmail.com>
2017-07-19 02:16:29 -0400

    Added unsigned integer tests

--- tests/test.c
+++ tests/test.c
@@ -25,12 +25,18 @@ void test_ok(int passed, const char* test, const char* file, int line) {
 }
 
 #define IS_OK(exp) do{ test_ok((exp), #exp, __FILE__, __LINE__); } while(0)
+static char byte_int_bytes[]  = {1,4,1}; // 0x01
+static char short_int_bytes[] = {1,2,5,2}; // 0x0201
+static char int_bytes[]       = {1,2,3,4,6,4}; // 0x04030201
+static char long_int_bytes[]  = {1,2,3,4,5,6,7,8,7,8}; // 0x0100000000000000
+static char byte_neg_int_bytes[]  = {0xff,4,1}; // 0xff
+
+static char byte_uint_bytes[]  = {0xff,8,1}; // 0xff
+static char short_uint_bytes[] = {1,0xff,9,2}; // 0xff01
+static char uint_bytes[]       = {1,2,3,0xff,10,4}; // 0x04030201
+static char long_uint_bytes[]  = {1,2,3,4,5,6,7,0xff,11,8}; // 0x0100000000000000
 
 void int_tests() {
-    static char byte_int_bytes[]  = {1,4,1}; // 0x01
-    static char short_int_bytes[] = {1,2,5,2}; // 0x0201
-    static char int_bytes[]       = {1,2,3,4,6,4}; // 0x04030201
-    static char long_int_bytes[]  = {1,2,3,4,5,6,7,8,7,8}; // 0x0100000000000000
     int64_t num = 0;
     SNF_flexb_ref root;
     IS_OK(snf_flexb_set_root(byte_int_bytes, 3, &root) == 0);
@@ -49,11 +55,41 @@ void int_tests() {
     IS_OK(snf_flexb_as_int64(&root, &num) == 0);
     IS_OK(num == 0x0807060504030201);
 
+    IS_OK(snf_flexb_set_root(byte_neg_int_bytes, 3, &root) == 0);
+    IS_OK(snf_flexb_as_int64(&root, &num) == 0);
+    IS_OK(num == -1);
+
+    IS_OK(snf_flexb_set_root(byte_uint_bytes, 3, &root) == 0);
+    IS_OK(snf_flexb_as_int64(&root, &num) == 0);
+    IS_OK(num == 0xff);
+
+
+}
+
+void uint_tests() {
+    int64_t num = 0;
+    SNF_flexb_ref root;
+    IS_OK(snf_flexb_set_root(byte_uint_bytes, 3, &root) == 0);
+    IS_OK(snf_flexb_as_int64(&root, &num) == 0);
+
+    IS_OK(snf_flexb_set_root(short_uint_bytes, 4, &root) == 0);
+    IS_OK(snf_flexb_as_int64(&root, &num) == 0);
+    IS_OK(num == 0xff01);
+
+    IS_OK(snf_flexb_set_root(uint_bytes, 6, &root) == 0);
+    IS_OK(snf_flexb_as_int64(&root, &num) == 0);
+    IS_OK(num == 0xff030201);
+
+    IS_OK(snf_flexb_set_root(long_uint_bytes, 10, &root) == 0);
+    IS_OK(snf_flexb_as_int64(&root, &num) == 0);
+    IS_OK(num == 0xff07060504030201);
+
 }
 
 int main() {
     int results = 0;
     int_tests();
+    uint_tests();
 
     if (tests_failed) {
         results = 1;
